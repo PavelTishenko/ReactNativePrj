f4498af71a59b2afddb9b990c445d965f405d13b
Admin <you@example.com>
2020-07-10 01:26:47 +0300

    Check

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
+      <TextInput placeholder="Enter data"/>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Нарешті запустив цю діч</Text>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
