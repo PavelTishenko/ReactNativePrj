143d18599c891f0eea87bf56f74c322a58b612d3
Admin <you@example.com>
2020-07-11 13:53:45 +0300

    add navbar with some props

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {Navbar} from './src/components/navbar'
+
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  
   return (
-    
     <View style={styles.container}>
-      <TextInput placeholder="Enter data"/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Нарешті запустив цю діч</Text>
-      <StatusBar style="auto" />
+      <Navbar title="Todo App"/>
     </View>
-    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+ 
 });
